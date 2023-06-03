599e35f151e307fa985ea1ca62fb8f2f18b0e0ba
Paulo Diego <paulo.diego@adm.unip.br>
2023-06-03 08:57:12 -0300

    Interface

---
+++ cs-go-angular/src/app/models/responses/card.ts
@@ -0,0 +1,13 @@
+export interface Card {
+    id: string;
+    name: string;
+    description: string;
+    weapon: string;
+    pattern: string;
+    min_float: number;
+    max_float: number;
+    rarity: string;
+    stattrak: boolean;
+    paint_index: string;
+    image: string;   
+}
