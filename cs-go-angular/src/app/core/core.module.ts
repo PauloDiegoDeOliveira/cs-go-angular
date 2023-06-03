f443312ad9e335fe154275aa6a1bef50b3d4981d
Paulo Diego <paulo.diego@unip.br>
2023-06-03 14:10:08 -0300

    Style padrão

--- cs-go-angular/src/app/core/core.module.ts
+++ cs-go-angular/src/app/core/core.module.ts
@@ -16,7 +16,7 @@ import { SharedModule } from '../shared/shared.module';
   ],
   exports: [
     NavbarComponent,
-    FooterComponent,   
+    FooterComponent,
   ]
 })
 export class CoreModule { }
