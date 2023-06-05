555ef484706be29240f690f39ab439511801dce2
Paulo Diego <paulo.diego@adm.unip.br>
2023-06-05 15:41:04 -0300

    Clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './views/home/home.module';
 
 @NgModule({
