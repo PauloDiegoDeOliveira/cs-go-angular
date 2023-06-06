d03abdae9fad10ada3a000f0f7e8d8cca96f25c2
Paulo Diego <paulo.diego@adm.unip.br>
2023-06-06 19:23:03 -0300

    NgFor

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,32 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Card } from 'src/app/models/responses/card';
-import { CardService } from 'src/app/services/card.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
 
-  cards: Card[] = [];  
+export class CardComponent {
 
-  constructor(private cardService: CardService) { }  
-
-  ngOnInit() {
-    this.getAllCards();  
-  }
-
-getAllCards() {
-    this.cardService.getAll<Card>('/skins.json').subscribe((data: Card) => {
-        if (Array.isArray(data)) {
-          this.cards = data;
-        } 
-        else {
-          console.error('Não foi possível obter os dados.');
-        }
-      },
-      (error) => console.error(error)
-    );
-  }
+  @Input() card: Card | undefined;
 }
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Card } from 'src/app/models/responses/card';
+import { CardService } from 'src/app/services/card.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+cards: Card[] = [];  
+
+  constructor(private cardService: CardService) { }  
+
+  ngOnInit() {
+    this.getAllCards();  
+  }
+
+getAllCards() {
+    this.cardService.getAll<Card>('/skins.json').subscribe((data: Card) => {
+        if (Array.isArray(data)) {
+          this.cards = data;
+        } 
+        else {
+          console.error('Não foi possível obter os dados.');
+        }
+      },
+      (error) => console.error(error)
+    );
+  }
 
 }
