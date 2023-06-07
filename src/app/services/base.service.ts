db6d884d666b1db81ea0f9ebb5606b259a081b87
Paulo Diego <paulo.diego@adm.unip.br>
2023-06-07 12:03:34 -0300

    Base inicial

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
 
-  constructor() { }
+protected urlApi: string = environment.urlApi;
+
+constructor(private http: HttpClient) { }
+
+protected getData<T>(url: string): Observable<T> {
+    return this.http.get<any>(`${this.urlApi}${url}`)
+  } 
+
 }
