abdd3019dcb0afef78dbb7b0469a568370b9f3a6
Paulo Diego <pdiegodo@gmail.com>
2024-07-11 15:08:16 -0300

    Ajustado responses

--- src/app/models/responses/card.ts
+++ src/app/models/responses/card.ts
@@ -1,13 +1,23 @@
 export interface Card {
+  id: string;
+  name: string;
+  description: string;
+  weapon: {
     id: string;
     name: string;
-    description: string;
-    weapon: string;
-    pattern: string;
-    min_float: number;
-    max_float: number;
-    rarity: string;
-    stattrak: boolean;
-    paint_index: string;
-    image: string;
+  };
+  pattern: {
+    id: string;
+    name: string;
+  };
+  min_float: number;
+  max_float: number;
+  rarity: {
+    id: string;
+    name: string;
+    color: string;
+  };
+  stattrak: boolean;
+  paint_index: string;
+  image: string;
 }
